4862707acf8e0f355e21616b0bc6b4e4fc5dc909
Muhammad Ahsen <muhammadahsen987@gmail.com>
2026-07-31 16:32:34 +0500

    Initial Setup through Spring Initializer

---
+++ src/main/java/dev/ahsen/Contact_Management_System/ContactManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.ahsen.Contact_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ahsen/Contact_Management_System/ContactManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ahsen.Contact_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
