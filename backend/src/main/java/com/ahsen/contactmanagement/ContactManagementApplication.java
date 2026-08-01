a8514432217ed1137afc5eca2ed1e64f88551aec
Muhammad Ahsen <muhammadahsen987@gmail.com>
2026-08-01 10:29:26 +0500

    Fix issues

--- backend/src/main/java/com/ahsen/contactmanagement/ContactManagementApplication.java
+++ backend/src/main/java/com/ahsen/contactmanagement/ContactManagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ContactmanagementApplication {
+public class ContactManagementApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ContactmanagementApplication.class, args);
+        SpringApplication.run(ContactManagementApplication.class, args);
     }
 
 }
