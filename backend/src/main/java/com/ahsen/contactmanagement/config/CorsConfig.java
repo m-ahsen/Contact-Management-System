dc210cf7c91ea7d74d818621e9c81577ea61d060
Muhammad Ahsen <muhammadahsen987@gmail.com>
2026-08-07 16:50:02 +0500

    Fix second coderabbit issue

--- backend/src/main/java/com/ahsen/contactmanagement/config/CorsConfig.java
+++ backend/src/main/java/com/ahsen/contactmanagement/config/CorsConfig.java
@@ -2,10 +2,12 @@ package com.ahsen.contactmanagement.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile("dev")
 public class CorsConfig {
 
     @Bean
